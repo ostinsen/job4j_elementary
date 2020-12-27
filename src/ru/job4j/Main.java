ad0b9bbef5a93bfe4e7e8faa008a157dec5cae3f
ostinsen <ostinsen@yandex.ru>
2020-12-27 11:06:36 +0300

    0. Что такое тестирование [#434820]

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -3,5 +3,26 @@ package ru.job4j;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        long l = 129;
+        byte b = (byte) l;
+        System.out.println(b);
+
+        float f = 12358.7f;
+        int i = 45981;
+        short rsl = (short) (f + i);
+        System.out.println(rsl);
+
+        char c = 45000;
+        float ii = c;
+        System.out.println(ii);
+
+        double d = 121.19;
+        byte bb = (byte) d;
+        System.out.println(bb);
+
+        short s = 1500;
+        char cc = (char) s;
+        double dd = cc;
+        System.out.println(dd);
     }
 }
--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -17,5 +17,17 @@ public class Converter {
         int dollar = Converter.rubleToDollar(180);
         System.out.println("180 rubles are " + dollar + " dollar");
 
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+
+        int inDol = 180;
+        int expectedDol = 3;
+        int outDol = Converter.rubleToDollar(inDol);
+        boolean passedDol = expectedDol == outDol;
+        System.out.println("180 rubles are 3 dollars. Test result : " + passedDol);
+
     }
 }
