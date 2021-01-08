20e1cb356944c878125233e8f194eea3f893aa7e
ostinsen <ostinsen@yandex.ru>
2021-01-08 16:23:24 +0300

    1. Экзамен. Базовый синтаксис. [#434832]

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -7,21 +7,53 @@ public class Merge {
     public static int[] merge(int[] left, int[] right) {
 
         int[] all = new int[left.length + right.length];
-        int count = 0;
         for (int i = 0; i < left.length; i++) {
-            all[i] = left[i];
-            count++;
+           all[i] = left[i];
         }
-        for (int j = 0; j < right.length; j++) {
-            all[count++] = right[j];
+        for (int i = 0; i < right.length; i++) {
+            for (int j = 0; j < all.length; j++) {
+                if (right[i] <= all[j]){
+                    for (int k = all.length - 1; k > j; k--) {
+                        all[k] = all[k - 1];
+                    }
+                    all[j] = right[i];
+                    break;
+                } else if ((all[j] == 0) && (j >= left.length - 1)){
+                    for (int k = all.length - 1; k > j; k--) {
+                        all[k] = all[k - 1];
+                    }
+                    all[j] = right[i];
+                    break;
+                }
+            }
         }
-        int[] array = new int[all.length];
-        for (int i = 0; i < all.length; i++) {
-            int min = MinDiapason.findMin(all, i, all.length - 1);
-            int index = FindLoop.indexOf(all, min, i, all.length - 1);
-            array = SwitchArray.swap(all, index, i);
-            /* swap(...) */
-        }
-        return array;
+
+
+//        int[] all = new int[left.length + right.length];
+//        int count = 0;
+//        for (int i = 0; i < left.length; i++) {
+//            all[i] = left[i];
+//            count++;
+//        }
+//        for (int j = 0; j < right.length; j++) {
+//            all[count++] = right[j];
+//        }
+//        int[] array = new int[all.length];
+//        for (int i = 0; i < all.length; i++) {
+//            int min = MinDiapason.findMin(all, i, all.length - 1);
+//            int index = FindLoop.indexOf(all, min, i, all.length - 1);
+//            array = SwitchArray.swap(all, index, i);
+//            /* swap(...) */
+//        }
+        return all;
     }
+
+//    public static void main(String[] args) {
+//        int[] arr2 = new int[] {0, 0, 10};
+//        int[] arr1 = new int[] {2, 5, 8, 8};
+//        int[] arr3 = Merge.merge(arr1, arr2);
+//            for (int i : arr3){
+//                System.out.println(i);
+//            }
+//    }
 }
